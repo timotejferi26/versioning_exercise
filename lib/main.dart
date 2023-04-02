8e55d67061777b11c47d0d889d6e4ba7e4390744
Timotej <timotej.sekeres@student.um.si>
2023-04-02 20:24:50 +0200

    added task list UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:versioning_exercise/providers/task_list_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,32 +12,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Task List',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Tasks'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends ConsumerStatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  ConsumerState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends ConsumerState<MyHomePage> {
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  TextEditingController nameController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -43,25 +41,93 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: ref.watch(taskListProvider).when(
+          data: (taskList) {
+            return ListView.builder(
+              itemCount: taskList.length,
+              itemBuilder: (context, index) {
+                return CheckboxListTile(
+                  title: Text(taskList[index].name),
+                  subtitle: Text(taskList[index].description),
+                  value: taskList[index].completed,
+                  onChanged: (bool? value) {
+                    ref.read(taskListProvider.notifier).switchCompleted(taskList[index].id);
+                  },
+                );
+              },
+            );
+          },
+          error: (error, stack) {
+            return ExceptionHandlerWidget(exception: error,);
+          },
+          loading: () => const Center(child: CircularProgressIndicator(),)
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Add task'),
+                content: Column(
+                  children: [
+                    TextField(
+                      controller: nameController,
+                      decoration: const InputDecoration(hintText: "Name"),
+                    ),
+                    TextField(
+                      controller: descriptionController,
+                      decoration: const InputDecoration(hintText: "Description"),
+                    ),
+                  ],
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      nameController.clear();
+                      descriptionController.clear();
+                    },
+                  ),
+                  TextButton(
+                    child: const Text('Add'),
+                    onPressed: () {
+                      ref.read(taskListProvider.notifier).addTask(
+                          nameController.text.trimRight(),
+                          descriptionController.text.trimRight());
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        tooltip: 'Add Task',
         child: const Icon(Icons.add),
-      ),
+      )
+    );
+  }
+}
+
+class ExceptionHandlerWidget extends StatelessWidget {
+  const ExceptionHandlerWidget({Key? key, required this.exception}) : super(key: key);
+
+  final Object exception;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(bottom: 20.0),
+          child: Icon(Icons.error_outline, color: Colors.red, size: 40,),
+        ),
+        Text(exception.toString())
+      ],
     );
   }
 }
